be44a1552e62cbb40859208d046cf5fdef64005f
martialdrogba-beep <jeanyvesdrogba@gmail.com>
2026-07-04 17:42:14 +0000

    Mes code
    Mes premiers code python avec pydroid 3

---
+++ newfile.py
@@ -0,0 +1,23 @@
+
+# recolter une premiere note
+note1 = int (input (" entrer la premiere note"))
+# recolter une second note 
+note2 = int(input ("entrer la second note"))
+# recolter la dernier note 
+note3 = int(input ("entrer la dernier note"))
+# calculer la moyen  result  = (note1 + note2 + note3)  
+result =  (note1 + note2 + note3)  /3
+# afficher la resultat
+print("la  moyen de l’élève est de  " + str (result  ))
+
+
+
+
+
+
+
+
+
+
+
+
